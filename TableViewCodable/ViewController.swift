79f4f489b6ee35c249a9129ef05dba30486d19ec
Marcin Karski <karski80@gmail.com>
2019-02-11 20:31:35 +0000

    Initial Commit

---
+++ TableViewCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCodable
+//
+//  Created by Marcin Karski on 11/02/2019.
+//  Copyright © 2019 Marcin Karski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
