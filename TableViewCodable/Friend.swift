49aaec5e3deb4bd938df79d8f0518c7099314b20
Marcin Karski <karski80@gmail.com>
2019-02-13 22:36:48 +0000

    Working

---
+++ TableViewCodable/Friend.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+typealias Friends = Friend
+
+struct Friend: Codable {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Connection]
+}
+
+struct Connection: Codable {
+    var id: UUID
+    var name: String
+}
--- TableViewCodable/ViewController.swift
+++ TableViewCodable/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var friends = [Friends]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -9,5 +11,33 @@ class ViewController: UITableViewController {
     
     private func setup() {
         self.title = "TableView Codable"
+        DispatchQueue.global().async {
+            do {
+                let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let downloaded = try decoder.decode([Friends].self, from: data)
+                DispatchQueue.main.async {
+                    self.friends = downloaded
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        let friend = friends[indexPath.row]
+        cell.textLabel?.text = friend.name
+        cell.detailTextLabel?.text = friend.friends.map { $0.name }.joined(separator: ", ")
+        return cell
     }
 }
